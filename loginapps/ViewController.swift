73bf24d911b1aa20230bccb76f8159a4ba77e3fb
https://github.com/Eigles/Swift.git <https://github.com/Eigles/Swift.git>
2014-07-06 18:26:01 +0200

    apprentissage

--- loginapps/ViewController.swift
+++ loginapps/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var theTextFieldUsername: UITextField;
+    
+    @IBOutlet var theTextFieldPassword: UITextField;
+    
+    @IBOutlet var thelabel: UILabel;
+    
+    
+    @IBAction func theVerifyMethod(sender: AnyObject)
+    {
+        var user:String = "toto";
+        var password:String = "toto";
+        if(theTextFieldUsername.text == user && theTextFieldPassword.text == password)
+        {
+            thelabel.text = "correcte";
+            thelabel.textColor = UIColor.greenColor();
+            theTextFieldUsername.resignFirstResponder();
+            theTextFieldPassword.resignFirstResponder();
+        }
+        else
+        {
+            thelabel.text = "erreur";
+            thelabel.textColor = UIColor.redColor();
+            theTextFieldUsername.resignFirstResponder();
+            theTextFieldPassword.resignFirstResponder();
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
